310a67c41856dd94600dcc60db51942166f6e4c3
StephanieTanner13 <80174471+StephanieTanner13@users.noreply.github.com>
2022-02-25 13:45:14 -0700

    week 1 part 2

---
+++ src/app/contacts/contact-detail/contact-detail.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+
+import { Contact } from '../contact.model';
+
+@Component({
+  selector: 'cms-contact-detail',
+  templateUrl: './contact-detail.component.html',
+  styleUrls: ['./contact-detail.component.css']
+})
+export class ContactDetailComponent implements OnInit {
+  contacts: Contact[] = [
+    new Contact("1" , 'R. Kent Jackson', 'jacksonk@byui.edu', '208-496-3771', '../../assets/images/jacksonk.jpg', null),
+    new Contact("2" , 'Rex Barzee', 'barzeer@byui.edu', '208-496-3768', '../../assets/images/barzeer.jpg', null)
+  ];
+
+  
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
