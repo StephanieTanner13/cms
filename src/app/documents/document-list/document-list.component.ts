2319bd8cf2821a8b5506217758f524a44cb5bb9c
StephanieTanner13 <80174471+StephanieTanner13@users.noreply.github.com>
2022-03-02 20:41:29 -0700

    Assignment 2.2

--- src/app/documents/document-list/document-list.component.ts
+++ src/app/documents/document-list/document-list.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+
+import { Document } from '../document.model';
 
 @Component({
   selector: 'cms-document-list',
@@ -6,10 +8,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./document-list.component.css']
 })
 export class DocumentListComponent implements OnInit {
+  @Output() selectedDocumentEvent = new EventEmitter<Document>();
+
+  documents: Document[] = [
+    new Document("1", "Here is a document", "this is a description", "http://stephtanner.com", null),
+    new Document("2", "Another Document", "this is a description also", "http://stephtanner.com", null),
+    new Document("3", "Nammmeee", "this is a description", "http://stephtanner.com", null),
+    new Document("4", "A Final Name", "this is a description", "../../assets/images/barzeer.jpg", null)
+  ];
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  onSelectedDocument(document: Document){
+    this.selectedDocumentEvent.emit(document);
+  }
 }
--- src/app/documents/documents.component.ts
+++ src/app/documents/documents.component.ts
@@ -1,12 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Document } from '../documents/document.model';
+
 @Component({
   selector: 'cms-documents',
   templateUrl: './documents.component.html',
   styleUrls: ['./documents.component.css']
 })
 export class DocumentsComponent implements OnInit {
-
+  selectedDocument: Document;
+  
   constructor() { }
 
   ngOnInit(): void {
